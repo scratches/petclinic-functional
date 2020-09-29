84fed1ac5fadfb7a807d9856ae8fe4c755788b7d
Dave Syer <dsyer@vmware.com>
2020-09-29 17:40:43 +0100

    Add visit endpoints

--- src/main/java/org/springframework/samples/petclinic/owner/PetController.java
+++ src/main/java/org/springframework/samples/petclinic/owner/PetController.java
@@ -15,32 +15,43 @@
  */
 package org.springframework.samples.petclinic.owner;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.ModelMap;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.format.support.FormattingConversionService;
 import org.springframework.util.StringUtils;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.ServletRequestDataBinder;
 import org.springframework.web.bind.WebDataBinder;
-import org.springframework.web.bind.annotation.*;
-
-import javax.validation.Valid;
-import java.util.Collection;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.servlet.function.RouterFunction;
+import org.springframework.web.servlet.function.RouterFunctions;
+import org.springframework.web.servlet.function.ServerRequest;
+import org.springframework.web.servlet.function.ServerResponse;
 
 /**
  * @author Juergen Hoeller
  * @author Ken Krebs
  * @author Arjen Poutsma
  */
-@Controller
-@RequestMapping("/owners/{ownerId}")
+@Configuration(proxyBeanMethods = false)
 class PetController {
 
     private static final String VIEWS_PETS_CREATE_OR_UPDATE_FORM = "pets/createOrUpdatePetForm";
     private final PetRepository pets;
     private final OwnerRepository owners;
+    private FormattingConversionService conversionService;
 
-    public PetController(PetRepository pets, OwnerRepository owners) {
+    public PetController(PetRepository pets, OwnerRepository owners,
+            @Qualifier("mvcConversionService") FormattingConversionService conversionService) {
         this.pets = pets;
         this.owners = owners;
+        this.conversionService = conversionService;
     }
 
     @ModelAttribute("types")
@@ -48,62 +59,93 @@ class PetController {
         return this.pets.findPetTypes();
     }
 
-    @ModelAttribute("owner")
-    public Owner findOwner(@PathVariable("ownerId") int ownerId) {
+    private Owner findOwner(@PathVariable("ownerId") int ownerId) {
         return this.owners.findById(ownerId);
     }
 
-    @InitBinder("owner")
-    public void initOwnerBinder(WebDataBinder dataBinder) {
-        dataBinder.setDisallowedFields("id");
-    }
-
-    @InitBinder("pet")
-    public void initPetBinder(WebDataBinder dataBinder) {
+    private void initPetBinder(WebDataBinder dataBinder) {
         dataBinder.setValidator(new PetValidator());
+        dataBinder.setConversionService(conversionService);
     }
 
-    @GetMapping("/pets/new")
-    public String initCreationForm(Owner owner, ModelMap model) {
+    @Bean
+    public RouterFunction<ServerResponse> petRoutes() {
+        return RouterFunctions.route().path("/owners/{ownerId}", builder -> builder //
+                .GET("/pets/new", this::initCreationForm) //
+                .POST("/pets/new", this::processCreationForm) //
+                .GET("/pets/{petId}/edit", this::initUpdateForm) //
+                .POST("/pets/{petId}/edit", this::processUpdateForm) //
+        ).build();
+    }
+
+    private ServerResponse initCreationForm(ServerRequest request) {
+        Map<String, Object> model = new HashMap<>();
+        Owner owner = findOwner(Integer.valueOf(request.pathVariable("ownerId")));
         Pet pet = new Pet();
         owner.addPet(pet);
         model.put("pet", pet);
-        return VIEWS_PETS_CREATE_OR_UPDATE_FORM;
+        model.put("owner", owner);
+        return ServerResponse.ok().render(VIEWS_PETS_CREATE_OR_UPDATE_FORM, model);
     }
 
-    @PostMapping("/pets/new")
-    public String processCreationForm(Owner owner, @Valid Pet pet, BindingResult result, ModelMap model) {
-        if (StringUtils.hasLength(pet.getName()) && pet.isNew() && owner.getPet(pet.getName(), true) != null){
+    private ServerResponse processCreationForm(ServerRequest request) {
+        Map<String, Object> model = new HashMap<>();
+        BindingResult result = bindPet(model, request);
+        Pet pet = (Pet) model.get("pet");
+        Owner owner = (Owner) model.get("owner");
+        if (StringUtils.hasLength(pet.getName()) && pet.isNew()
+                && owner.getPet(pet.getName(), true) != null) {
             result.rejectValue("name", "duplicate", "already exists");
         }
-        owner.addPet(pet);
         if (result.hasErrors()) {
-            model.put("pet", pet);
-            return VIEWS_PETS_CREATE_OR_UPDATE_FORM;
-        } else {
+            return ServerResponse.ok().render(VIEWS_PETS_CREATE_OR_UPDATE_FORM, model);
+        }
+        else {
             this.pets.save(pet);
-            return "redirect:/owners/{ownerId}";
+            return ServerResponse.ok().render("redirect:/owners/{ownerId}", model);
         }
     }
 
-    @GetMapping("/pets/{petId}/edit")
-    public String initUpdateForm(@PathVariable("petId") int petId, ModelMap model) {
+    private ServerResponse initUpdateForm(ServerRequest request) {
+        Map<String, Object> model = new HashMap<>();
+        int petId = Integer.valueOf(request.pathVariable("petId"));
         Pet pet = this.pets.findById(petId);
         model.put("pet", pet);
-        return VIEWS_PETS_CREATE_OR_UPDATE_FORM;
+        return ServerResponse.ok().render(VIEWS_PETS_CREATE_OR_UPDATE_FORM, model);
     }
 
-    @PostMapping("/pets/{petId}/edit")
-    public String processUpdateForm(@Valid Pet pet, BindingResult result, Owner owner, ModelMap model) {
+    private ServerResponse processUpdateForm(ServerRequest request) {
+        Map<String, Object> model = new HashMap<>();
+        BindingResult result = bindPet(model, request);
+        Pet pet = (Pet) model.get("pet");
         if (result.hasErrors()) {
-            pet.setOwner(owner);
-            model.put("pet", pet);
-            return VIEWS_PETS_CREATE_OR_UPDATE_FORM;
-        } else {
-            owner.addPet(pet);
-            this.pets.save(pet);
-            return "redirect:/owners/{ownerId}";
+            return ServerResponse.ok().render(VIEWS_PETS_CREATE_OR_UPDATE_FORM, model);
         }
+        else {
+            this.pets.save(pet);
+            return ServerResponse.ok().render("redirect:/owners/{ownerId}", model);
+        }
+    }
+
+    private BindingResult bindPet(Map<String, Object> model, ServerRequest request) {
+        Pet pet = new Pet();
+        ServletRequestDataBinder binder = new ServletRequestDataBinder(pet, "pet");
+        initPetBinder(binder);
+        binder.bind(request.servletRequest());
+        binder.validate();
+        BindingResult result = binder.getBindingResult();
+        Integer ownerId = Integer.valueOf(request.pathVariable("ownerId"));
+        Owner owner = findOwner(ownerId);
+        ServletRequestDataBinder ownerBinder = new ServletRequestDataBinder(owner,
+                "owner");
+        model.put("ownerId", ownerId);
+        owner.addPet(pet);
+        model.put("owner", owner);
+        model.put("org.springframework.validation.BindingResult.owner",
+                ownerBinder.getBindingResult());
+        model.put("pet", pet);
+        model.put("org.springframework.validation.BindingResult.pet", result);
+        return result;
     }
 
 }
--- src/main/java/org/springframework/samples/petclinic/owner/VisitController.java
+++ src/main/java/org/springframework/samples/petclinic/owner/VisitController.java
@@ -67,9 +67,9 @@ class VisitController {
     }
 
     /**
-     * Called before each and every @RequestMapping annotated method. 2 goals: - Make sure
-     * we always have fresh data - Since we do not use the session scope, make sure that
-     * Pet object always has an id (Even though id is not part of the form fields)
+     * Called by every handler method. 2 goals: - Make sure we always have fresh data -
+     * Since we do not use the session scope, make sure that Pet object always has an id
+     * (Even though id is not part of the form fields)
      *
      * @param petId
      * @return Pet
